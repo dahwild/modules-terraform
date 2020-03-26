909a0cbc1da0ba22d56896f816a72dbf1726ef69
Wilder David <dahwild@gmail.com>
2020-03-26 12:40:49 -0500

    Creacion de modulos

---
+++ instance/variables.tf
@@ -0,0 +1,17 @@
+# Declarar varibles
+variable "ami_id" {
+    default = ""
+    description = "Ami Id"
+}
+variable "instance_type" {
+
+}
+variable "tags" {
+    type = map
+}
+variable "sg_name" {
+
+}
+variable "ingress_rule" {
+  
+}
